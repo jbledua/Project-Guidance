ba80f276ee801b560dd973361b501c4c8995c09b
Josiah Ledua <jledua@lakeheadu.ca>
2023-04-19 17:52:15 -0400

    Updated Inbox Page - Added simple Mat List

--- src/app/pages/inbox-page/inbox-page.component.ts
+++ src/app/pages/inbox-page/inbox-page.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./inbox-page.component.scss']
 })
 export class InboxPageComponent {
+  // Dummy data for messages, replace it with your actual data source
+  messages = [
+    { title: 'Message 1', content: 'This is the content of Message 1' },
+    { title: 'Message 2', content: 'This is the content of Message 2' },
+    { title: 'Message 3', content: 'This is the content of Message 3' },
+  ];
 
+  constructor() {}
+
+  createNewMessage(): void {
+    // Your logic for creating a new message, e.g., navigate to a new message form
+    console.log('Creating a new message');
+  }
 }
