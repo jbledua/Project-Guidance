b8c2bf39db220ffac22f4b93794c61763f551612
Josiah Ledua <jledua@lakeheadu.ca>
2023-04-18 21:31:09 -0400

    Created Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// Import the pages
 import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { InboxPageComponent } from './pages/inbox-page/inbox-page.component';
 
+// Import the AuthGuard
+import { AuthGuard } from './guards/auth.guard';
+
 
 const routes: Routes = [
-  { path: 'inbox', component: InboxPageComponent},
+  { path: 'inbox', component: InboxPageComponent,canActivate: [AuthGuard],},
   { path: 'login', component: LoginPageComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/inbox', pathMatch: 'full' },
   { path: '**', component: NotfoundPageComponent }
 ];
 
